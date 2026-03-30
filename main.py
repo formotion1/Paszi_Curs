ad8f0ccfeb4d2fa4a0dd175d9143e4a2869f27b3
formotion1 <132114826+formotion1@users.noreply.github.com>
2026-03-31 00:07:02 +0700

    Update main.py
    PRIVATE KEY

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ def get_hwid():
 
 
 # === 2. Проверка лицензии ===
-"""PUBLIC_KEY = b"""
+PRIVATE_KEY = b"""
 
-"""-----BEGIN PUBLIC KEY-----
+-----BEGIN PRIVATE KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkP6svES7LstL1dNWoxT/
 xbXERVHk1Bqoa1bIzAW3X0qflAnCL1QiYS/sN3W7V2dSZr4Q+3uMkWeUwG9i1n2Y
 agdyh06dOdP3Ia9mNBWTqLk5M8uwVVxEuQWkQLwGYyXwGvA20Z3/PuWjf8gL2U5Z
@@ -49,10 +49,10 @@ lwwtON0sBDPWJvLE+stGxBNGQGQ5/8pVXs7xQHs8e7JYz+qeTdcUn3bIOqbZtUt6
 IuNfgmSk+B11vwDVEHwkfJq11dFWH6wRDl8G6m8O3i6HtqLB22wQUKdY2JVU3VMH
 FQRg/jRQRCiyjUxL7KYeuaCPRi7d52zcRawNK089lxMXtk+xExji1yI0kkDaBW90
 CwIDAQAB
------END PUBLIC KEY-----
+-----END PRIVATE KEY-----
 """
-"""
-"""
+
+
 def verify_license(current_hwid):
     try:
         with open("license.json", "r") as f:
